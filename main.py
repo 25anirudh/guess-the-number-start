00e84c9e0d09d8a28ebff3a13a579569692a171e
25anirudh <126631015+25anirudh@users.noreply.github.com>
2023-03-01 00:59:39 +0000

    its a cool number guessing game

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-2#Number Guessing Game Objectives:
+
 from art import logo
 print(logo)
 import random
 easy_level_turns = 10
 hard_level_turns = 5
-# Include an ASCII art logo.
-# Allow the player to submit a guess for a number between 1 and 100.
 
-
-
-  # Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer.
 def check_answer(guess, actual, turn):
   if guess > actual:
     print("your guess is very high")
@@ -19,7 +14,6 @@ def check_answer(guess, actual, turn):
     return turn -1
   else :
     print("you guessed the correct number , you won")
-# If they got the answer correct, show the actual answer to the player.
 def set_difficulty():
   level = input("choose the difficulty level 'easy' or 'hard' : ")
   if level == "easy":
